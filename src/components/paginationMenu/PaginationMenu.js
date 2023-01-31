ef7ef3873d5ec8ceb7d925d33b13feb6de4c4136
marcinzygan <soul@o2.pl>
2023-01-31 19:42:50 +0100

    added paginationMenu component

---
+++ src/components/paginationMenu/PaginationMenu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./PaginationMenu.module.css";
+const PaginationMenu = () => {
+  return (
+    <div>
+      <button>first</button>
+      <button>next</button>
+      <button>prev</button>
+      <button>last</button>
+    </div>
+  );
+};
+
+export default PaginationMenu;
