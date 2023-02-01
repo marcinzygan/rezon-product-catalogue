a661bace6de5f7f6ccbf2635a40790f2be014883
marcinzygan <soul@o2.pl>
2023-02-01 18:19:55 +0100

    added dynamic img for the ProductCard

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -1,11 +1,18 @@
 import classes from "./productCard.module.css";
 import React from "react";
-
+import Image from "next/image";
 const ProductCard = (card) => {
+  console.log(card.image);
   return (
     <>
       <div className={classes.card}>
         <div>{card.identyfikator}</div>
+        <Image
+          src={card.image}
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        />
         <div>{card.opis}</div>
         <div>{card.cena}</div>
         <div>{card.id}</div>
