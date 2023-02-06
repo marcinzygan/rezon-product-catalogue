b57d31db4a4eb696cbd27e0ee0b189a6e99bc291
marcinzygan <soul@o2.pl>
2023-02-06 14:35:50 +0100

    added styles for nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 import FilterMenu from "../filterMenu/FilterMenu";
 import classes from "./navBar.module.css";
 
@@ -11,7 +12,21 @@ const NavBar = () => {
         <img className={classes.logo} src="/images/logo3.jpg"></img>
         <img className={classes.logo} src="/images/logo4.jpg"></img>
       </div>
-      <div className={classes.company__details}>DETAILS</div>
+      <div className={classes.company__details_container}>
+        <p>Rezon Sp. z.o.</p>
+        <div className={classes.adress}>
+          <Icon icon="ic:round-location-on" />{" "}
+          <p>ul. Ketlinga 1 , 78-100 Kołobrzeg</p>
+        </div>
+        <div className={classes.nip}>
+          <Icon icon="material-symbols:folder-open" />
+          <p>6711400398</p>
+        </div>
+        <div className={classes.phone}>
+          <Icon icon="material-symbols:phone-in-talk-watchface-indicator" />
+          <p>94 355 14 50</p>
+        </div>
+      </div>
       <FilterMenu />
     </div>
   );
