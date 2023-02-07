261728205b8321693e761fa9ab4843f264a3c3a4
marcinzygan <soul@o2.pl>
2023-02-07 16:00:03 +0100

    changed store folder , renamed magnesy folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout.js";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { store } from "../state/store/store.js";
 
 export default function App({ Component, pageProps }) {
   return (
