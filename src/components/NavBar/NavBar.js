2634792794cc2ec3ff4afc36153807a9ed727841
marcinzygan <soul@o2.pl>
2023-02-07 08:48:06 +0100

    added category images

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,32 +5,34 @@ import classes from "./navBar.module.css";
 
 const NavBar = () => {
   return (
-    <div className={classes.nav}>
-      <div className={classes.logo__container}>
-        <img className={classes.logo} src="/images/logo1.jpg"></img>
-        <img className={classes.logo} src="/images/logo2.jpg"></img>
-        <img className={classes.logo} src="/images/logo3.jpg"></img>
-        <img className={classes.logo} src="/images/logo4.jpg"></img>
-      </div>
-      <div className={classes.menuDetails__wrapper}>
-        <div className={classes.company__details_container}>
-          <p>Rezon Sp. z.o.</p>
-          <div className={classes.adress}>
-            <Icon icon="ic:round-location-on" />{" "}
-            <p>ul. Ketlinga 1 , 78-100 Kołobrzeg</p>
-          </div>
-          <div className={classes.nip}>
-            <Icon icon="material-symbols:folder-open" />
-            <p>6711400398</p>
-          </div>
-          <div className={classes.phone}>
-            <Icon icon="material-symbols:phone-in-talk-watchface-indicator" />
-            <p>94 355 14 50</p>
-          </div>
+    <nav className={classes.nav__container}>
+      <div className={classes.nav}>
+        <div className={classes.logo__container}>
+          <img className={classes.logo} src="/images/logo1.jpg"></img>
+          <img className={classes.logo} src="/images/logo2.jpg"></img>
+          <img className={classes.logo} src="/images/logo3.jpg"></img>
+          <img className={classes.logo} src="/images/logo4.jpg"></img>
         </div>
-        <FilterMenu />
+        <div className={classes.menuDetails__wrapper}>
+          <div className={classes.company__details_container}>
+            <p>Rezon Sp. z o.o</p>
+            <div className={classes.adress}>
+              <Icon icon="ic:round-location-on" />{" "}
+              <p>ul. Ketlinga 1 , 78-100 Kołobrzeg</p>
+            </div>
+            <div className={classes.nip}>
+              <Icon icon="material-symbols:folder-open" />
+              <p>6711400398</p>
+            </div>
+            <div className={classes.phone}>
+              <Icon icon="material-symbols:phone-in-talk-watchface-indicator" />
+              <p>94 355 14 50</p>
+            </div>
+          </div>
+          <FilterMenu />
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
