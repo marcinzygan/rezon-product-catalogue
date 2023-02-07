b298294dff69f81e852b0e911d7d31030b454258
marcinzygan <soul@o2.pl>
2023-02-07 08:34:00 +0100

    added styles to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -12,22 +12,24 @@ const NavBar = () => {
         <img className={classes.logo} src="/images/logo3.jpg"></img>
         <img className={classes.logo} src="/images/logo4.jpg"></img>
       </div>
-      <div className={classes.company__details_container}>
-        <p>Rezon Sp. z.o.</p>
-        <div className={classes.adress}>
-          <Icon icon="ic:round-location-on" />{" "}
-          <p>ul. Ketlinga 1 , 78-100 Kołobrzeg</p>
+      <div className={classes.menuDetails__wrapper}>
+        <div className={classes.company__details_container}>
+          <p>Rezon Sp. z.o.</p>
+          <div className={classes.adress}>
+            <Icon icon="ic:round-location-on" />{" "}
+            <p>ul. Ketlinga 1 , 78-100 Kołobrzeg</p>
+          </div>
+          <div className={classes.nip}>
+            <Icon icon="material-symbols:folder-open" />
+            <p>6711400398</p>
+          </div>
+          <div className={classes.phone}>
+            <Icon icon="material-symbols:phone-in-talk-watchface-indicator" />
+            <p>94 355 14 50</p>
+          </div>
         </div>
-        <div className={classes.nip}>
-          <Icon icon="material-symbols:folder-open" />
-          <p>6711400398</p>
-        </div>
-        <div className={classes.phone}>
-          <Icon icon="material-symbols:phone-in-talk-watchface-indicator" />
-          <p>94 355 14 50</p>
-        </div>
+        <FilterMenu />
       </div>
-      <FilterMenu />
     </div>
   );
 };
