69a243dbe15eb65ed1d1902d0fde63fe9d8283ad
marcinzygan <soul@o2.pl>
2023-02-10 13:19:14 +0100

    added Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./footer.module.css";
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <img className={classes.footer__logo} src="/images/logo4.jpg"></img>
+    </footer>
+  );
+};
+
+export default Footer;
