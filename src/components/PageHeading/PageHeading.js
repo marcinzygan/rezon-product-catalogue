4cff9d6a8d35edfe6aefb71af8d1003246f17938
marcinzygan <soul@o2.pl>
2023-02-27 13:13:13 +0100

    added subHeading component

--- src/components/PageHeading/PageHeading.js
+++ src/components/PageHeading/PageHeading.js
@@ -1,8 +1,13 @@
 import React from "react";
 import classes from "./pageHeading.module.css";
 
-const PageHeading = (heading) => {
-  return <div>{heading}</div>;
+const PageHeading = (props) => {
+  return (
+    <div>
+      <h2 className={classes.heading}>{props.heading}</h2>
+      <p className={classes.subHeading}>{props.subHeading}</p>
+    </div>
+  );
 };
 
 export default PageHeading;
--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,9 +1,11 @@
 import GoogleMap from "@/components/GoogleMap/GoogleMap";
+import PageHeading from "@/components/PageHeading/PageHeading";
 import React from "react";
 
 const kontakt = () => {
   return (
     <div>
+      <PageHeading heading={"Kontakt"} subHeading={"Jak możemy pomóc ?"} />
       <GoogleMap />
     </div>
   );
